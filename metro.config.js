91522598fb41933ddd29dad7fcb455c937a51266
João Mathias <jvlmathias@gmail.com>
2025-07-31 03:11:04 -0300

    SDK Fixed :white_check_mark:

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,15 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+// Adicionar configurações para melhor compatibilidade
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+config.transformer.minifierConfig = {
+  keep_fnames: true,
+  mangle: {
+    keep_fnames: true,
+  },
+};
+
+module.exports = config;
